f3afb984d9adefacd61350663357b2130b78be7a
sohelranaweb <sohel.rana.web2@gmail.com>
2023-09-25 22:49:37 +0600

    all dontion card show from localStorage

--- src/components/Donation/Donation.jsx
+++ src/components/Donation/Donation.jsx
@@ -1,7 +1,22 @@
+import { useEffect, useState } from "react";
+import DonationPage from "../DonationPage/DonationPage";
+
 const Donation = () => {
+  const [donations, setDonations] = useState([]);
+
+  useEffect(() => {
+    const donationItems = JSON.parse(localStorage.getItem("donations"));
+    if (donationItems) {
+      setDonations(donationItems);
+    }
+  }, []);
   return (
     <div>
-      <h1>This is Donation page</h1>
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+        {donations.map((donation) => (
+          <DonationPage key={donation.id} donation={donation}></DonationPage>
+        ))}
+      </div>
     </div>
   );
 };
---
+++ src/components/DonationPage/DonationPage.jsx
@@ -0,0 +1,61 @@
+const DonationPage = ({ donation }) => {
+  const {
+    id,
+    picture,
+    title,
+    category,
+    price,
+    card_bg_color,
+    category_bg_color,
+    text_button_bg_color,
+  } = donation;
+  return (
+    <div>
+      <div
+        style={{ backgroundColor: card_bg_color }}
+        className="relative flex w-full max-w-[48rem] flex-row rounded-xl shadow-md"
+      >
+        <div className="relative m-0 w-2/5 shrink-0 overflow-hidden rounded-xl rounded-r-none bg-white bg-clip-border text-gray-700">
+          <img
+            src={picture}
+            alt="image"
+            className="h-full w-full object-cover"
+          />
+        </div>
+        <div className="p-6 space-y-2">
+          <button
+            className="px-4 py-2 rounded-md text-sm font-medium"
+            style={{
+              backgroundColor: category_bg_color,
+              color: text_button_bg_color,
+            }}
+            type="button"
+            data-ripple-light="true"
+          >
+            {category}
+          </button>
+          <h1
+            className=" text-xl font-semibold"
+            style={{ color: text_button_bg_color }}
+          >
+            {title}
+          </h1>
+          <p
+            className="text-base font-semibold"
+            style={{ color: text_button_bg_color }}
+          >
+            {price}
+          </p>
+          <button
+            className="px-4 py-2 text-white text-lg font-semibold rounded-md"
+            style={{ backgroundColor: text_button_bg_color }}
+          >
+            View Details
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DonationPage;
