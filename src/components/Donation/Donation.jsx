43906d2b9a8a5c0da420ce4bbe88e638a98b961c
sohelranaweb <sohel.rana.web2@gmail.com>
2023-09-26 09:13:36 +0600

    banner and serch field funtion add

--- src/components/Donation/Donation.jsx
+++ src/components/Donation/Donation.jsx
@@ -3,6 +3,7 @@ import DonationPage from "../DonationPage/DonationPage";
 
 const Donation = () => {
   const [donations, setDonations] = useState([]);
+  const [dataLength, setDataLength] = useState(4);
 
   useEffect(() => {
     const donationItems = JSON.parse(localStorage.getItem("donations"));
@@ -11,11 +12,19 @@ const Donation = () => {
     }
   }, []);
   return (
-    <div>
+    <div className="my-10">
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
-        {donations.map((donation) => (
+        {donations.slice(0, dataLength).map((donation) => (
           <DonationPage key={donation.id} donation={donation}></DonationPage>
         ))}
+      </div>
+      <div className={dataLength === donations.length ? "hidden " : ""}>
+        <button
+          onClick={() => setDataLength(donations.length)}
+          className="px-4 py-2 bg-[#009444] text-white block mx-auto mt-10 rounded-md"
+        >
+          See All
+        </button>
       </div>
     </div>
   );
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,14 +1,59 @@
 import { useLoaderData } from "react-router-dom";
 import DonationData from "../DonationData/DonationData";
+import { useState } from "react";
 
 const Home = () => {
-  const donationDatas = useLoaderData();
+  const allDonationData = useLoaderData();
+  console.log(allDonationData);
+  const [searchItem, setSearchItem] = useState("");
+  const filteredDonationData = allDonationData.filter((donation) => {
+    // Check if the donation's category or any other relevant field matches the search term
+    return donation.category.toLowerCase().includes(searchItem.toLowerCase());
+    // ||
+    // donation.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+    // donation.description.toLowerCase().includes(searchTerm.toLowerCase())
+  });
+
+  const handleSearchInputChange = (event) => {
+    setSearchItem(event.target.value);
+  };
 
   return (
     <div>
-      <h1>Banner section here</h1>
+      <div className="mb-24">
+        <div
+          className="hero h-[80vh]"
+          style={{
+            backgroundImage:
+              "url(https://i.ibb.co/kDnJ4q8/donation-banner.jpg)",
+          }}
+        >
+          <div className="hero-overlay bg-[#FFFFFFF3]"></div>
+          <div className="hero-content text-center text-neutral-content">
+            <div className="">
+              <h1 className="mb-5 text-5xl font-bold text-[#0B0B0B]">
+                I Grow By Helping People In Need
+              </h1>
+              <div className="form-control">
+                <div className="input-group flex justify-center">
+                  <input
+                    type="text"
+                    placeholder="Search…"
+                    className="input input-bordered text-black"
+                    value={searchItem}
+                    onChange={handleSearchInputChange}
+                  />
+                  <button className="btn bg-[#FF444A] text-white">
+                    Search
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
-        {donationDatas.map((donation) => (
+        {filteredDonationData.map((donation) => (
           <DonationData key={donation.id} donation={donation}></DonationData>
         ))}
       </div>
